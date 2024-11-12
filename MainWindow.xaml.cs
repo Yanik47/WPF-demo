7a7d3ce943d9c3890c08572fb9b73d09fc1a14de
Yanik47 <95187012+Yanik47@users.noreply.github.com>
2024-11-12 11:24:29 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace GraficInterfacePresentation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleFlowDirection_Click(object sender, RoutedEventArgs e)
+        {
+            // Переключение FlowDirection между RightToLeft и LeftToRight
+            this.FlowDirection = this.FlowDirection == FlowDirection.LeftToRight
+                ? FlowDirection.RightToLeft
+                : FlowDirection.LeftToRight;
+        }
+    }
+}
